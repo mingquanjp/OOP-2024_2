5a20dae905ed031bab54b2a6248fcf3a2775304a
mingquanjp <faker1652005@gmail.com>
2025-03-23 01:00:14 +0700

    commit for Lab02

---
+++ Lab02/src/lab02/Aims.java
@@ -0,0 +1,20 @@
+package lab02;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dv1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers",87, 19.95f);
+        anOrder.addDigitalVideoDisc(dv1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        //Total cost
+        System.out.println(anOrder.totalCost());
+
+    }
+}
--- Lab02/src/lab02/Cart.java
+++ Lab02/src/lab02/Cart.java
@@ -4,7 +4,7 @@ public class Cart {
 
     private int qtyOrdered =0;
     public static final int MAX_NUMBERS_ORDERED =20;
-    private  DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private  DigitalVideoDisc itemOrdered [] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
 
     //add DVD to the cart
     public void addDigitalVideoDisc(DigitalVideoDisc item){
@@ -18,13 +18,36 @@ public class Cart {
     }
 
     //remove DVD to cart
+    boolean found =false;
     public void removeDigitalVideoDisc(DigitalVideoDisc item){
         if ( qtyOrdered ==0){
             System.out.println("The cart has no DVD");
         }else{
-            itemOrdered[--qtyOrdered]=item;
+            //search the disc
+            for (int i=0; i<qtyOrdered;i++){
+                if(itemOrdered[i].equals(item)){
+                    found =true;
+                    //If DVD match, take all the dvd right of itemOrder[i] to the left
+                    for(int j=i; j< qtyOrdered -1; j++){
+                        itemOrdered[j] = itemOrdered[j+1];
+                    }
+                    itemOrdered[qtyOrdered-1] = null;
+                    qtyOrdered--;
+                    System.out.println("The DVD with title " + item.getTitle() + " has been removed from the cart");
+                }
+            }
+            if(!found){
+                System.out.println("The DVD is not match with any DVD in the cart");
+            }
         }
 
 
+    }
+    float total =0;
+    public float totalCost(){
+        for (int i=0; i< qtyOrdered;i++){
+            total+=itemOrdered[i].getCost();
+        }
+        return total;
     }
 }
